e4b3be04de5e3300e3566a5b61df51b5a052751d
Henix212 <hugogpdealmeida@gmail.com>
2025-05-18 07:33:09 +0200

    Monster Style

--- views/monster.php
+++ views/monster.php
@@ -1,20 +1,35 @@
 <?php
     $this->layout('templates', ['title' => 'La Guilde']);
+    $listMonsters = [
+        "public/images/assets/babayaga.png",
+        "public/images/assets/banshee.png",
+        "public/images/assets/basilic.png",
+        "public/images/assets/corbeau.png",
+        "public/images/assets/demon.png",
+        "public/images/assets/draugr.png",
+        "public/images/assets/espritforet.png",
+        "public/images/assets/goblin.png",
+        "public/images/assets/gremlin.png",        
+        "public/images/assets/leshi.png",
+        "public/images/assets/loup.png"
+    ];
 ?>
 
-<div class="container">
-    <div class="slide">
-        <?php foreach ($listUnits as $unit): ?> <!-- On parcourt les unités -->
-            <div class="item" style="background-image: url('<?= htmlspecialchars($monster->getName()) ?>');"> <!-- On affiche l'image de l'unité dans la div directement-->
-                <div class="monster-content">
-                    
+<link rel="stylesheet" href="public/css/monster.css">
+<script src="public/js/monsters.js"></script>
+
+<main id="contenu">
+    <div class="container">
+        <div class="slide">
+            <?php foreach ($listMonsters as $monster): ?>
+                <div class="item" style="background-image: url('<?= htmlspecialchars($monster) ?>');">
+                    <div class="monster-content"></div>
                 </div>
-            </div>
-        <?php endforeach; ?>  
+            <?php endforeach; ?>  
+        </div>
+        <div class="lr_button">
+            <button id="left-button"><img src="./public/images/fleche-gauche.png" alt="Précédent"></button>
+            <button id="right-button"><img src="./public/images/fleche-droite.png" alt="Suivant"></button>
+        </div>
     </div>
-    <div class="lr_button">
-        <button id="left-button"><img src="./public/img/fleche-gauche.png" alt="Précédent"></button> <!-- On crée un bouton pour aller à la slide précédente -->
-        <button id="right-button"><img src="./public/img/fleche-droite.png" alt="Suivant"></button> <!-- On crée un bouton pour aller à la slide suivante -->
-    </div>
-</div>
-</div>
+</main>
