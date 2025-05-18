dbac2897d3b153a4ee0333dec53e682ee28416c1
Henix212 <hugogpdealmeida@gmail.com>
2025-05-18 06:54:02 +0200

    New style for profile

--- views/profil.php
+++ views/profil.php
@@ -6,10 +6,17 @@ use Config\Config;
 
 $this->layout("templates", ["title" => Config::get("title")]);
 
+// Générateur de numéro aléatoire
+$randomNumber = random_int(0, 3);
+
 ?>
 
+<link rel="stylesheet" href="public/css/profil.css">
+
 <div class="profil">
-    <h1>Profil :</h1>
+    <div class="container-photo-profile">
+        <img src="public/images/assets/joueur<?= $randomNumber ?>.png" alt="Photo de profil" class="photo-profile">
+    </div>
     <h3><?= $name ?></h3>
     <h3><?= $surname ?></h3>
     <h4><?= $status ?></h4>
