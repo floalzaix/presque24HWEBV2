82e86d9d4388dae001ebbdce75bc494e57df3e91
Henix212 <hugogpdealmeida@gmail.com>
2025-05-18 06:33:39 +0200

    new css for auth

--- views/AuthView.php
+++ views/AuthView.php
@@ -10,10 +10,13 @@ $this->layout("templates", ["title" => Config::get("title")]);
 
 <link rel="stylesheet" href="public/css/auth.css">
 
-<div class="auth_content">
-    <form action="index.php?action=auth" method="POST">
-        <input type="text" name="auth_login" id="auth_login" placeholder="Identifiant" />
-        <input type="password" name="auth_pwd" id="auth_pwd" placeholder="Mot de passe" />
-        <input type="submit" name="submit_button" id="submit_button" />
-    </form>
+<div class="auth_center">
+    <div class="auth_content">
+        <h1 class="title-login">Sign-in</h1>
+        <form action="index.php?action=auth" method="POST">
+            <input type="text" name="auth_login" id="auth_login" placeholder="Identifiant" />
+            <input type="password" name="auth_pwd" id="auth_pwd" placeholder="Mot de passe" />
+            <input type="submit" name="submit_button" id="submit_button" />
+        </form>
+    </div>
 </div>
